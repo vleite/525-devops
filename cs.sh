8a8961b028f35481e613ae8fea514d5ccc88d6eb
Paramahansa Yogananda <paramahansa_yogananda@paramahansayogananda.com>
2019-04-18 23:17:50 +0000

    Alterando cor para amarelo

--- cs.sh
+++ cs.sh
@@ -3,7 +3,9 @@ count='0'
 while [ "$count" -lt 20 ]
 do
   clear
+  echo -e '\033[33;1m'
   cowsay -f $(ls /usr/share/cowsay/cows/ | shuf -n1) 'Paramahansa Yogananda'
+  echo -e '\e[m'
   sleep 10
   count=$(expr $count + 1)
 done
